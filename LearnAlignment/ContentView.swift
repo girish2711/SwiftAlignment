3c22f98fbbae54607a2cadb5ea5cbab6ba3b1747
Girish Adiga <girish2711@users.noreply.github.com>
2021-01-03 09:28:12 -0500

    Learning swift alignments and alignment guids

---
+++ LearnAlignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnAlignment
+//
+//  Created by Girish Adiga on 1/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnAlignment/LearnAlignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAlignmentApp.swift
+//  LearnAlignment
+//
+//  Created by Girish Adiga on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnAlignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
